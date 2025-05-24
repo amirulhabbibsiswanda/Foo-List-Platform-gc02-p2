733ca76a5685e450756fc0de7e93ef463dbc7391
amirulhabbibsiswanda <amirulhabbib458@gmail.com>
2025-05-24 18:17:09 +0700

    chore: all filter

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -17,7 +17,19 @@ function App() {
 
   const [dropDown, setDropDown] = useState(false)
   const [categories, setCategories] = useState([])
+  const [filterCategory, setFilterCategory] = useState("")
 
+
+  async function fetchCategories() {
+    try {
+      const { data } = await axiosInstance.get("/apis/pub/restaurant-app/categories")
+      // console.log(data.data, "ini categories");
+      setCategories(data.data)
+    } catch (error) {
+      console.log(error, "ini error di cateogries");
+
+    }
+  }
 
   async function fetchCuisines() {
     try {
@@ -44,6 +56,14 @@ function App() {
           path = path + `?sort=${sort}`
         }
       }
+
+      if (filterCategory) {
+        if (search || page || sort) {
+          path = path + `&i=${filterCategory}`
+        } else {
+          path = path + `i=${filterCategory}`
+        }
+      }
       const { data } = await axiosInstance.get(path)
       setCuisines(data.data.query)
 
@@ -53,22 +73,21 @@ function App() {
       // console.log(data.data.query[0].Category, "kategori");
       // console.log(data.data.query, "cui");
 
-      let arrCategories = []
-      for (let i = 0; i < data.data.query.length; i++) {
-        arrCategories.push(data.data.query[i].Category.name)
-      }
-      // console.log(arrCategories, "arr kategori  ");
-      setCategories(arrCategories)
     } catch (error) {
       console.log(error);
     }
   }
 
+  // useEffect(() => {
+  //   fetchCategories()
+  // }, [])
+
   useEffect(() => {
     // console.log("jalan use effect============");
     fetchCuisines()
+    fetchCategories()
     // console.log(totalPage, "total page di use effect");
-  }, [search, page, sort])
+  }, [search, page, sort, filterCategory])
 
   const renderButton = () => {
     // console.log(totalPage, "ini totalpage setelah diset");
@@ -82,68 +101,91 @@ function App() {
     return arrButton
 
   }
+
+  const handleCategoryButton = () => {
+    if (dropDown) {
+      setDropDown(false)
+    } else {
+      setDropDown(true)
+    }
+  }
+
+  const handleFilterCategory = (categoryName) => {
+    setDropDown(false)
+    setFilterCategory(categoryName)
+  }
+
+  const clearFilter = () => {
+    setSearch("")
+    setPage(1)
+    setSort("")
+    setFilterCategory("")
+  }
   return (
     <>
-      {console.log(categories)
-      }
+
       <Navbar />
-      <>
-        <button
-          id="dropdownDefaultButton"
-          data-dropdown-toggle="dropdown"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          type="button"
-        >
-          Dropdown button{" "}
-          <svg
-            className="w-2.5 h-2.5 ms-3"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 10 6"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="m1 1 4 4 4-4"
-            />
-          </svg>
-        </button>
-        {/* Dropdown menu */}
-        <div
-          id="dropdown"
-          className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700"
-        >
-          <ul
-            className="py-2 text-sm text-gray-700 dark:text-gray-200"
-            aria-labelledby="dropdownDefaultButton"
-          >
-            <li>
-              <a
-                href="#"
-                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-              >
-                Dashboard
-              </a>
-            </li>
+      {/* {JSON.stringify(categories)} */}
+      <div className="flex items-start justify-between gap-4 px-4 py-4">
+        <>
+          <div className="relative">
+            <button
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              type="button"
+              onClick={handleCategoryButton}
+            >
+              Categories
+            </button>
 
-          </ul>
+            {/* Dropdown menu */}
+            {dropDown && (
+              <div className="absolute mt-2 bg-amber-200 rounded-lg shadow w-36 flex flex-col gap-1 z-10">
+                {categories.map((el) => (
+                  <p
+                    key={el.id}
+                    onClick={() => handleFilterCategory(el.name)}
+                    className=" bg-amber-300 hover:cursor-pointer hover:bg-amber-500 rounded"
+                  >
+                    {el.name}
+                  </p>
+                ))}
+              </div>
+            )}
+          </div>
+        </>
+
+
+
+        <div className="flex flex-col items-center gap-2 self-start">
+          <button
+            onClick={() => setSort("desc")}
+            className="bg-amber-200 rounded-lg px-3 py-1 hover:bg-amber-300"
+          >
+            Newest Cuisine
+          </button>
+          <button
+            onClick={() => setSort("asc")}
+            className="bg-amber-200 rounded-lg px-3 py-1 hover:bg-amber-300"
+          >
+            Latest Cuisine
+          </button>
         </div>
-      </>
 
+        <div className="self-start">
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-yellow-300"
+            placeholder="search by cuisine name"
+          />
+        </div>
 
-
-
-
-      <div className='gap-2 my-2 flex flex-col container justify-center place-items-center'>
-        <button onClick={() => { setSort("desc") }} className='bg-amber-200 rounded-lg px-1'>Newest Cuisine</button>
-        <button onClick={() => { setSort("asc") }} className='bg-amber-200 rounded-lg px-1'>Latest Cuisine</button>
+        <button onClick={clearFilter} className='hover:cursor-pointer bg-red-200 rounded px-4 py-2 self-start'>
+          Clear Filter</button>
       </div>
-      <div>
-        <input type="text" value={search} onChange={(e) => { setSearch(e.target.value) }} className='border' placeholder='search by cuisine name' />
-      </div>
+
+
       <div className='mx-5 flex justify-evenly flex-wrap'>
         {
           cuisines.map((cuisine) => {
